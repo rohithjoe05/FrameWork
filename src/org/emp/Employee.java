0583db65d9d8a58c20a39927ba4b4318e088c971
rohithjoe05 <rohithjoe05@gmail.com>
2022-09-02 12:17:04 +0530

    FrameWork Ref

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,30 @@
+package org.emp;
+
+import org.client.Client;
+import org.company.Company;
+import org.project.Project;
+
+public class Employee {
+	
+	private void empName() {
+		System.out.println("EmpName-Ramesh");
+
+	}
+	public static void main(String[] args) {
+		
+		Employee e = new Employee();
+		e.empName();
+		
+		Company c = new Company();
+		c.companyName();
+		
+		Client d = new Client();
+		d.clientName();
+		
+		Project p = new Project();
+		p.projectName();
+		
+	
+	}
+
+}
